56cb23e4564b2e3a16b119d201e69370356f8e97
shuetfong <shuetfongcheang@gmail.com>
2025-01-26 12:46:47 +0800

    Add web analytics and speed insights components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -23,6 +25,8 @@ export default function RootLayout({
         className={`${geistMono.variable} antialiased bg-gray-950`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
